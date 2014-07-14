4f2ec09f2a1b8218852a38413314f14c00c36973
M. Kamal Khan <shout@bhittani.com>
2014-07-14 15:08:29 +0500

    Update func name

--- index.js
+++ index.js
@@ -17,16 +17,16 @@
  * @param   int              Iterator
  * @return  bool|string|int  Arg value on each recursion
  */
-(function bubble(i){
+(function green(i){
     if(!module.exports.args) module.exports.args = {};
     if(i >= process.argv.length) return true;
     var c = process.argv[i].match(/^-{1}(.+)$/);
     if(!c){
-        bubble(i+1);
+        green(i+1);
         var v = process.argv[i];
         return (!isNaN(v) && parseInt(v) == v)
         ? parseInt(v) : v;
     }
-    module.exports.args[c[1]] = bubble(i+1);
+    module.exports.args[c[1]] = green(i+1);
     return true;
 })(2);
